0dfc476314292cd6735aaaccc985509543743af0
nhannhan159 <tanquoctien@gmail.com>
2015-10-18 19:08:43 +0700

    - Fix membership provider had wrong connection string. - Success testing simple membership provider.

--- RoomM.WebApp/Filters/InitializeSimpleMembershipAttribute.cs
+++ RoomM.WebApp/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace RoomM.WebApp.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("RoomDB", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
